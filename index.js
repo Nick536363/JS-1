26e597e4f7f47d25287d7d6f87e3b2ef640fa56e
Nick536363 <134803463+Nick536363@users.noreply.github.com>
2024-12-15 10:16:58 +0100

    fixed "plus" function in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let form = document.getElementById("maForm");
 let div = document.getElementById("result");
 function plus(){
-    div.textContent=form.number1.value+form.number2.value
+    div.textContent=Number(form.number1.value)+Number(form.number2.value)
 }
 function minus(){
     div.textContent=form.number1.value-form.number2.value
