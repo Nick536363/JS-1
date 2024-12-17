3ffbe4e05c88521cc2494b207ddb2b500b4c3600
Nick536363 <134803463+Nick536363@users.noreply.github.com>
2024-12-17 17:20:55 +0100

    changed function and variable names

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 let form = document.getElementById("maForm");
-let div = document.getElementById("result");
-function plus(){
-    div.textContent=Number(form.number1.value)+Number(form.number2.value)
+let result = document.getElementById("result"); 
+function addition(){
+   result.textContent=Number(form.number1.value)+Number(form.number2.value)
 }
-function minus(){
-    div.textContent=form.number1.value-form.number2.value
+function substraction(){
+    result.textContent=form.number1.value-form.number2.value
 }
-function divide(){
-    div.textContent=form.number1.value/form.number2.value
+function division(){
+    result.textContent=form.number1.value/form.number2.value
 }
-function multiply(){
-    div.textContent=form.number1.value*form.number2.value
+function multiplication(){
+    result.textContent=form.number1.value*form.number2.value
 }
 
